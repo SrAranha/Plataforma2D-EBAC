44fb8846790acf147b926d399a322f5b26b38fe9
Danilo "Aranha <daniloprado7@gmail.com>
2023-02-13 17:28:36 -0300

    Adicionado animações ao jogador quando se move.
    As animações são controladas pelo script PlayerController.
    Nesse mesmo script, alterei a fricção manual para ser feita com uma variável editável no editor da Unity, ao invés do hardcoded de antes.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,18 +5,23 @@ public class PlayerController : MonoBehaviour
     [Header("Player Movement")]
     public float speed;
     public float speedRun;
+    public float manualFriction;
     public float jumpForce;
     [Min(1)]
     public int jumpsAmount;
+    [Header("Animation")]
+    public string runParam;
 
-    private Rigidbody2D rb2D;
-    private float currentSpeed;
-    //private bool isGrounded;
     private bool jump;
+    private Rigidbody2D rb2D;
     private int jumpCount = 0;
+    private Animator animator;
+    //private bool isGrounded;
+    private float currentSpeed;
 
     private void Awake()
     {
+        animator = GetComponentInChildren<Animator>();
         rb2D = GetComponent<Rigidbody2D>();
     }
     private void FixedUpdate()
@@ -42,18 +47,28 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             rb2D.velocity = new Vector2(-currentSpeed, rb2D.velocity.y);
+            animator.SetBool(runParam, true);
+            rb2D.transform.localScale = new Vector2(-1, 1);
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
             rb2D.velocity = new Vector2(currentSpeed, rb2D.velocity.y);
+            animator.SetBool(runParam, true);
+            rb2D.transform.localScale = new Vector2(1, 1);
         }
-        if (rb2D.velocity.x > .1f)
+        else 
         {
-            rb2D.velocity -= new Vector2(.1f, 0);
+            animator.SetBool(runParam, false);
         }
-        else if (rb2D.velocity.x < -.1f)
+
+        // Applying manual friction
+        if (rb2D.velocity.x > manualFriction)
         {
-            rb2D.velocity += new Vector2(.1f, 0);
+            rb2D.velocity -= new Vector2(manualFriction, 0);
+        }
+        else if (rb2D.velocity.x < -manualFriction)
+        {
+            rb2D.velocity += new Vector2(manualFriction, 0);
         }
     }
     private void HandleJump()
